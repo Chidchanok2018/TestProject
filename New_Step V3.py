2ec3f0539f1632b06b9621f74f27031bd351274c
Chidchanok2018 <42292882+Chidchanok2018@users.noreply.github.com>
2018-08-13 21:32:32 +0700

    Next_Step V3 RecheckDefinter

--- New_Step V3.py
+++ New_Step V3.py
@@ -7,41 +7,43 @@ import operator
 import random
 import numpy as np
 import pandas as pd
+
 G = nx.Graph()
 fh = open("C:\Users\Kmutt_Wan\PycharmProjects\simulated_blockmodel_graph_500_nodes_snowball_2.txt", "rb")
 G = read_adjlist(fh)
 
-#-------Original Graph--------#
+# -------Original Graph--------#
 Number_of_nodes = len(G.nodes)
 Number_of_Edges = len(G.edges)
 print'Number Of Nodes_Real', Number_of_nodes
 print'Number Of Edges_Real', Number_of_Edges
 Node_Degree = [i for i in G.degree]
 Max_Degree = int(max(Node_Degree[1]))
-Max_Degree_Show = int(max(Node_Degree[1]))+1
+Max_Degree_Show = int(max(Node_Degree[1])) + 1
 print 'Max Degree of Node', Max_Degree_Show
 Min_Degree = int(min(Node_Degree[1]))
-Min_Degree_Show = int(min(Node_Degree[1]))+1
+Min_Degree_Show = int(min(Node_Degree[1])) + 1
 print 'Min Degree of Node', Min_Degree_Show
 
-#----Check Highly Connection----------#
+# ----Check Highly Connection----------#
 print 'Highly Solution 1 = edge in graph > n / 2'
-if Number_of_Edges >= (Number_of_nodes/2):
-    print '= Yes ,', (Number_of_nodes/2)
+if Number_of_Edges >= (Number_of_nodes / 2):
+    print '= Yes ,', (Number_of_nodes / 2)
 else:
     print '= NOT'
 
-#--------เตรียม Sub cycles เ---------------#
+# --------เตรียม Sub cycles เ---------------#
 Sub3 = [c for c in nx.cycle_basis(G) if len(c) == 3]
 print'Len of Sub3', len(Sub3), 'Cycles'
 Sub4 = [c for c in nx.cycle_basis(G) if len(c) == 4]
 print'Len of Sub4', len(Sub4), 'Cycles'
 Sub_cycle3_sort = sorted(Sub3)
-#print'Sub_cycle3_MaDeg', Sub_cycle3_sort
+# print'Sub_cycle3_MaDeg', Sub_cycle3_sort
 Sub_cycle4_MaDeg = sorted(Sub4)
 
-#------Definition Function-------------#
-def Next_Scycle_N2(A_Subcycle,AN_Subcycle):
+
+# ------Definition Function-------------#
+def Next_Scycle_N2(A_Subcycle, AN_Subcycle):
     Scycle_same2 = []
     for h in A_Subcycle:
         count = len(A_Subcycle) - 1
@@ -52,10 +54,10 @@ def Next_Scycle_N2(A_Subcycle,AN_Subcycle):
             print'StartScycle', Start_Scycle
             Next_Scycle = set(AN_Subcycle[i + 1])
             print'NextScycle', Next_Scycle
-            a = Start_Scycle & Next_Scycle #เอาที่เหมือน
+            a = Start_Scycle & Next_Scycle  # เอาที่เหมือน
             print'StartAndNext', a
-            #b = Start_Scycle - Next_Scycle #เอาที่ไม่เหมือน
-            #print'StartOrNext', b
+            # b = Start_Scycle - Next_Scycle #เอาที่ไม่เหมือน
+            # print'StartOrNext', b
             if len(a) >= 2.0:
                 Merge_Sub = Start_Scycle | Next_Scycle
                 Scycle_same2.append(Next_Scycle)
@@ -65,16 +67,16 @@ def Next_Scycle_N2(A_Subcycle,AN_Subcycle):
                 print'[]'
         return Scycle_same2
 
-def interintra(A_Sub_N2,AN_Sub_N2):
+def interintra(A_Sub_N2, AN_Sub_N2):
     for o in A_Sub_N2:
         Cluster = []
         Keep = []
-        count = len(A_Sub_N2) - 1
-        for o1 in range(count):
+        count = len(A_Sub_N2) - 1  # 6
+        for o1 in range(count):  # 6
             print'-----------inter-intra----------------'
             Start_Scycle = set(o)
-            if len(Cluster) > 3:
-                Start_Scycle = Cluster
+            if len(Cluster) >= 2:
+                Start_Scycle = Start_Scycle | a
             print'Start_Scycle =', Start_Scycle
             Next_Scycle = set(AN_Sub_N2[o1 + 1])
             print'Next_Scycle =', Next_Scycle
@@ -93,7 +95,7 @@ def interintra(A_Sub_N2,AN_Sub_N2):
                 Number_of_Edes_All = float(len(a)) + Source
                 N = float(len(a))
                 N_C = float(len(a))
-                if (N_C*(N-N_C)) <= 0.00:
+                if (N_C * (N - N_C)) <= 0.00:
                     interN4 = 0.00
                     Re_inter4 = interN4
                 elif Number_of_Edges_Out <= 0.00:
@@ -116,20 +118,24 @@ def interintra(A_Sub_N2,AN_Sub_N2):
                     Re_intra4 = round(intra_2, 2)
                 print 'Re_intra4', " %+2.2f" % Re_intra4
                 Dif_Den_N2 = Re_intra4 - Re_inter4
-                print 'Difference Density =',  " %+2.2f" % Dif_Den_N2
+                print 'Difference Density =', " %+2.2f" % Dif_Den_N2
             if Dif_Den_N2 >= 0.15:
-                Cluster = Next_Scycle | a
+                if Start_Scycle not in Cluster:
+                    if len(Start_Scycle) <= 3:
+                        Cluster.append(Start_Scycle)
+                Cluster.append(Next_Scycle)
             else:
                 Keep.append(Next_Scycle)
         return Cluster
 
-def S3N2interintraCut(Re_S3N2_interintra,Compare_Sub,Compare_S3_N2):
-    for o in Compare_Sub:
+
+def S3N2interintraCut(Re_S3N2_interintra, Compare_Sub, Compare_S3_N2):
+    for o in Re_S3N2_interintra:
         Scycle_same2 = []
-        count = len(Compare_Sub) - 1
+        count = len(Re_S3N2_interintra) - 1
         for o1 in range(count):
             print'--------Cut_Scycle-------------'
-            Start_Scycle = Re_S3N2_interintra
+            Start_Scycle = set(o)
             print'StartScycle', Start_Scycle
             Next_Scycle = set(Compare_Sub[o1 + 1])
             print'NextScycle', Next_Scycle
@@ -143,7 +149,8 @@ def S3N2interintraCut(Re_S3N2_interintra,Compare_Sub,Compare_S3_N2):
                 print'[]'
         return Scycle_same2
 
-def ReinterintraAllSub(Re_S3N2_interintra,Compare_S3N2_Cut):
+
+def ReinterintraAllSub(Re_S3N2_interintra, Compare_S3N2_Cut):
     for o in Compare_S3N2_Cut:
         Cluster = []
         Keep = []
@@ -154,9 +161,9 @@ def ReinterintraAllSub(Re_S3N2_interintra,Compare_S3N2_Cut):
             if len(Cluster) > 3:
                 Start_Scycle = Cluster
             print'StartScycle', Start_Scycle
-            if o1 == count: #1
+            if o1 == count :  # 1
                 Next_Scycle = set(Compare_S3N2_Cut[0])
-            if o1 < count:
+            if o1 < count - 1:
                 Next_Scycle = set(Compare_S3N2_Cut[o1 + 1])
             print'NextScycle', Next_Scycle
             a = Start_Scycle | Next_Scycle
@@ -174,7 +181,7 @@ def ReinterintraAllSub(Re_S3N2_interintra,Compare_S3N2_Cut):
                 Number_of_Edes_All = float(len(a)) + Source
                 N = float(len(a))
                 N_C = float(len(a))
-                if (N_C*(N-N_C)) <= 0.00:
+                if (N_C * (N - N_C)) <= 0.00:
                     interN4 = 0.00
                     Re_inter4 = interN4
                 elif Number_of_Edges_Out <= 0.00:
@@ -197,27 +204,27 @@ def ReinterintraAllSub(Re_S3N2_interintra,Compare_S3N2_Cut):
                     Re_intra4 = round(intra_2, 2)
                 print 'Re_intra4', " %+2.2f" % Re_intra4
                 Dif_Den_N2 = Re_intra4 - Re_inter4
-                print 'Difference Density =',  " %+2.2f" % Dif_Den_N2
+                print 'Difference Density =', " %+2.2f" % Dif_Den_N2
             if Dif_Den_N2 >= 0.15:
                 Cluster = Next_Scycle | a
             else:
                 Keep.append(Next_Scycle)
         return Start_Scycle
 
-#--------------Main Program----------------
-S3N2 = Next_Scycle_N2(Sub3, Sub_cycle3_sort)
-S3N2_Sorted = sorted(S3N2)
-S3N2_interR1 = interintra(S3N2, S3N2_Sorted)
-S3N2_interR1_Cut = S3N2interintraCut(S3N2_interR1,Sub3,S3N2)
-S3N2_interR2 = ReinterintraAllSub(S3N2_interR1,S3N2_interR1_Cut)
 
+# --------------Main Program----------------
+S3N2 = Next_Scycle_N2(Sub3, Sub_cycle3_sort)  # เอาเฉพาะที่เหมือนกัน 2 โหนด
+S3N2_Sorted = sorted(S3N2)  # จัดเรียง cycles ที่เหมือนกัน 2 โหนดใหม่
+S3N2_interR1 = interintra(S3N2, S3N2_Sorted)  # คำนวน interintra รอบที่ cycles เหมือนกัน 2 โหนด
+S3N2_interR1_Cut = S3N2interintraCut(S3N2_interR1, Sub3, S3N2)  # ตัดที่ใช้แล้วออกจาก Sub 3
+S3N2_interR2 = ReinterintraAllSub(S3N2_interR1, S3N2_interR1_Cut)  #  คำนวน inter จากที่ตัดออกมา
 
-#--------------Print Result----------------
+# --------------Print Result----------------
 print'S3N2', S3N2
 print'Len S3N2 =', len(S3N2)
 print'S3N2_interR1 =', S3N2_interR1
+print'Len S3N2_interR1 =', len(S3N2_interR1)
 print'S3N2_interR1_Cut =', S3N2_interR1_Cut
 print'Len S3N2_interR1_Cut =', len(S3N2_interR1_Cut)
 print'S3N2_interR2 =', S3N2_interR2
-print'Len_S3N2_interR2 =', len(S3N2_interR2)
-
+#print'Len_S3N2_interR2 =', len(S3N2_interR2)
